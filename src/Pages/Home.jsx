5e00e968aab64875f1408104abe37ed146f7a803
Anmol8611 <kumaranmol8611@gmail.com>
2023-09-10 10:14:10 +0530

    final design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <main className='min-h-screen min-w-screen flex flex-col items-center justify-center gap-16'>
         <section className='flex flex-col gap-5'>
-            <h1 className='font-bold text-5xl text-sky-500'>Welcome to BizConnect</h1>
+            <h1 className='font-bold text-5xl text-sky-500'>Welcome to <span className='text-slate-700 text-7xl'>BizConnect</span></h1>
             <p className='font-light max-w-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sint consectetur nihil obcaecati iure aliquid expedita porro natus, repudiandae molestiae.</p>
         </section>
-        <section className='flex items-center gap-20'>
+        <section className='flex items-center gap-32'>
             <Link to={`/listing/${`volunteers`}`}>
             <BizCard title={`Hire Volunteers`} image={``}/>
             </Link>
